a8c66e2001c60bf6a02582bda15fafc5106241d8
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-01-13 04:57:27 +0700

    membuat file DestinasiNavigasi.kt dan fungsinya

---
+++ app/src/main/java/com/example/terserah/ui/home/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,17 @@
+package com.example.terserah.ui.home.navigation
+
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
